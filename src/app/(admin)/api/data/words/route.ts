24221077c277218f548158866ec1e6828786a1d2
kaanygit <yasinkaan345@hotmail.com>
2023-09-16 04:02:59 +0300

    fix the bugs

--- src/app/(admin)/api/data/words/route.ts
+++ src/app/(admin)/api/data/words/route.ts
@@ -1,6 +1,6 @@
-import startDatabase from "@/lib/database";
-import { WordModelA1, WordModelA2, WordModelB1,WordModelB2 } from "@/lib/models/word-model";
 import { NextResponse } from "next/server";
+import startDatabase from "../../../../../lib/database";
+import { WordModelA1, WordModelA2, WordModelB1,WordModelB2 } from "../../../../../lib/models/word-model";
 
 export async function GET(req:any){
     await startDatabase();
